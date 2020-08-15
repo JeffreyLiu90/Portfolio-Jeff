2f2cd8cc3ccdfb9f4591a3b20310d62245ade22a
Jeffrey <jeffrey.liu90@gmail.com>
2020-08-14 23:01:35 -0400

    main items added on to portfolio, now need to edit it

--- src/js/main.js
+++ src/js/main.js
@@ -26,33 +26,33 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener("click", linkAction));
 
-// /*===== SCROLL REVEAL ANIMATION =====*/
-// const sr = ScrollReveal({
-//   origin: "top",
-//   distance: "80px",
-//   duration: 2000,
-//   reset: true
-// });
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true
+});
 
-// /*SCROLL HOME*/
-// sr.reveal(".home__title", {});
-// sr.reveal(".button", { delay: 200 });
-// sr.reveal(".home__img", { delay: 400 });
-// sr.reveal(".home__social-icon", { interval: 200 });
+/*SCROLL HOME*/
+sr.reveal(".home__title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home__img", { delay: 400 });
+sr.reveal(".home__social-icon", { interval: 200 });
 
-// /*SCROLL ABOUT*/
-// sr.reveal(".about__img", {});
-// sr.reveal(".about__subtitle", { delay: 400 });
-// sr.reveal(".about__text", { delay: 400 });
+/*SCROLL ABOUT*/
+sr.reveal(".about__img", {});
+sr.reveal(".about__subtitle", { delay: 400 });
+sr.reveal(".about__text", { delay: 400 });
 
-// /*SCROLL SKILLS*/
-// sr.reveal(".skills__subtitle", {});
-// sr.reveal(".skills__text", {});
-// sr.reveal(".skills__data", { interval: 200 });
-// sr.reveal(".skills__img", { delay: 600 });
+/*SCROLL SKILLS*/
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__text", {});
+sr.reveal(".skills__data", { interval: 200 });
+sr.reveal(".skills__img", { delay: 600 });
 
-// /*SCROLL WORK*/
-// sr.reveal(".work__img", { interval: 200 });
+/*SCROLL WORK*/
+sr.reveal(".work__img", { interval: 200 });
 
-// /*SCROLL CONTACT*/
-// sr.reveal(".contact__input", { interval: 200 });
+/*SCROLL CONTACT*/
+sr.reveal(".contact__input", { interval: 200 });
