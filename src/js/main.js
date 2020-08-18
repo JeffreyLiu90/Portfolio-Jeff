1605cc2dca4eb470fe2b0deb6330250381d6ce98
Jeffrey <jeffrey.liu90@gmail.com>
2020-08-17 23:48:54 -0400

    added a new skills sections

--- src/js/main.js
+++ src/js/main.js
@@ -49,9 +49,8 @@ sr.reveal(".about__text", { delay: 400 });
 
 /*SCROLL SKILLS*/
 sr.reveal(".skills__subtitle", {});
-sr.reveal(".skills__text", {});
-sr.reveal(".skills__data", { interval: 200 });
-sr.reveal(".skills__img", { delay: 600 });
+sr.reveal(".skills__name", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills__img", { delay: 400 });
 
 /*SCROLL WORK*/
 // sr.reveal(".work__img", { interval: 200 });
