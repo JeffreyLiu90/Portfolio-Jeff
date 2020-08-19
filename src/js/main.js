169d47b52160b06fdfe2686c044826b3b920efca
Jeffrey <jeffrey.liu90@gmail.com>
2020-08-19 00:27:30 -0400

    changed the scroll for home second title

--- src/js/main.js
+++ src/js/main.js
@@ -38,6 +38,7 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal(".home__title", {});
+sr.reveal(".home-second-title", {});
 sr.reveal(".button", { delay: 200 });
 sr.reveal(".home__img", { delay: 400 });
 sr.reveal(".home__social-icon", { interval: 200 });
